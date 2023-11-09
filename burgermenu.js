71158296c418de6de5453f3f83bd3c3ada41d29c
emilerenault <150397258+emilerenault@users.noreply.github.com>
2023-11-10 00:43:27 +0100

    Portfolio

---
+++ burgermenu.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const nav = document.getElementById('nav');
+    const links = document.querySelectorAll('nav li');
+    const icons = document.getElementById('icons');
+
+    icons.addEventListener("click", () => {
+        nav.classList.toggle("active");
+    });
+
+    links.forEach((link) => {
+        link.addEventListener("click", () => {
+            nav.classList.remove("active");
+        });
+    });
+});
